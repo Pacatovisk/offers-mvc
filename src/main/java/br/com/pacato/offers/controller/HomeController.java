9c27d536ce5d485e07d0bb86bed4152a053bcc0a
marcelo.dias <marcelovianadias@outlook.com>
2021-03-24 10:22:22 -0300

    percorrendo a lista de pedidos na view

--- src/main/java/br/com/pacato/offers/controller/HomeController.java
+++ src/main/java/br/com/pacato/offers/controller/HomeController.java
@@ -1,14 +1,27 @@
 package br.com.pacato.offers.controller;
 
+import br.com.pacato.offers.model.Pedido;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String home(){
+    public String home(Model model){
+        Pedido pedido = new Pedido();
+        pedido.setNomeProduto("Vitrola");
+        pedido.setUrlImagem("https://www.google.com");
+        pedido.setUrlProduto("https://www.omni.com");
+        pedido.setDescricao("Produto de qualidade");
+
+        List<Pedido> pedidos = Arrays.asList(pedido);
+        model.addAttribute("pedidos", pedidos);
+
         return "home";
     }
 }
