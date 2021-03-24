c97398437101c041aaaf3930cf3f86b5faf8736e
marcelo.dias <marcelovianadias@outlook.com>
2021-03-24 15:25:13 -0300

    adicionando getters e setters da classe RequisicaoNovoPedido

--- src/main/java/br/com/pacato/offers/dto/RequisicaoNovoPedido.java
+++ src/main/java/br/com/pacato/offers/dto/RequisicaoNovoPedido.java
@@ -24,4 +24,36 @@ public class RequisicaoNovoPedido {
         pedido.setDescricao(descricao);
         return pedido;
     }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getUrlProduto() {
+        return urlProduto;
+    }
+
+    public void setUrlProduto(String urlProduto) {
+        this.urlProduto = urlProduto;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
