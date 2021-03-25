2ebc48e2ce5023809f6ac6b0742fd0d37e3f480e
marcelo.dias <marcelovianadias@outlook.com>
2021-03-25 11:41:53 -0300

    configurando spring security

---
+++ src/main/java/br/com/pacato/offers/config/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package br.com.pacato.offers.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@Profile("test")
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder().username("admin").password("admin").roles("admin").build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
