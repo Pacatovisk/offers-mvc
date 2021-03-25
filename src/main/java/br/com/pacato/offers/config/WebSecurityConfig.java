178f9369094d7241c810239039dadebff198edeb
marcelo.dias <marcelovianadias@outlook.com>
2021-03-25 14:04:45 -0300

    implementando logout para login

--- src/main/java/br/com/pacato/offers/config/WebSecurityConfig.java
+++ src/main/java/br/com/pacato/offers/config/WebSecurityConfig.java
@@ -26,7 +26,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin(form -> form
                 .loginPage("/login")
                 .permitAll()
-                );
+                )
+                .logout(logout -> logout.logoutUrl("/logout"));
     }
 
     @Bean
